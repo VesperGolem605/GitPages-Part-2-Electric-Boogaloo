26d326751735870cbca863fe50e0da7391144a3f
VesperGolem605 <89645272+VesperGolem605@users.noreply.github.com>
2021-09-24 19:52:46 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,9 +2,11 @@ let myFunction = function() {
   var r = prompt("Please enter hello or goodbye");
   if (r === "hello") {
     for(var i = 0; i < document.getElementsByClassName("hello").length; i++){
+      alert("asd");
       var matches = i.innerHTML.match(/\d+$/);
-      if (r) {
-        var number = parseInt(r[0]);
+      alert(i.innerHTML);
+      if (matches) {
+        var number = parseInt(matches[0]);
         number *= 2;
         i.innerHTML = "Hello Number " + number;
       }
