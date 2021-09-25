3689f0fa7f9af47d7eb7bd7f95698f213e4d443f
VesperGolem605 <89645272+VesperGolem605@users.noreply.github.com>
2021-09-24 20:04:08 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,17 +1,8 @@
 let myFunction = function() {
   var r = prompt("Please enter hello or goodbye3");
   if (r === "hello") {
-    for(var i = 0; i < document.getElementsByClassName("hello").length; i++){
-      var q = prompt("Plasdgoodbye3")
-      var matches = i.innerHTML.match(/\d+$/);
-      alert(i.innerHTML);
-      if (matches) {
-        var number = parseInt(matches[0]);
-        number *= 2;
-        i.innerHTML = "Hello Number " + number;
-      }
-    }
-    var x = prompt("Plasdgoodbye3");
+      var r = prompt(" hello or goodbye3");
+    document.getElementsByClassName("hello").innerHTML = "asd";
   } else if(r === "goodbye") {
     
   } else {
