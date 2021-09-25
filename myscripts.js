9092e0c5beb3e8c25de7621a0dffde045df9fbe5
VesperGolem605 <89645272+VesperGolem605@users.noreply.github.com>
2021-09-24 20:27:40 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,9 @@
 let myFunction = function() {
   var r = prompt("Please enter hello or goodbye3");
   if (r === "hello") {
-    for(var i = 0; i < document.getElementsByClassName("hello").length; i++){
-      var matches =  document.getElementsByClassName("hello")[i].innerHTML.match(/\d+$/);
-      document.getElementsByClassName("hello")[i].innerHTML = ("Hello Number " + matches[0]*2);
-    }
+    document.getElementsByClassName("hello").style = highlight;
   } else if(r === "goodbye") {
-    for(var i = 0; i < document.getElementsByClassName("goodbye").length; i++){
-      var matches =  document.getElementsByClassName("goodbye")[i].innerHTML.match(/\d+$/);
-      document.getElementsByClassName("goodbye")[i].innerHTML = ("Goodbye Number " + matches[0]*2);
-    }
+    
   } else {
     alert("Incorrect, please enter hellow or goodbye");
   }
